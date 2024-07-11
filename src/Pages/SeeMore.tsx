f6edc81f10d51cb4f6f2d1b023eb43dedb4db82d
ifeanyihm <iheme.ifeanyi@yahoo.com>
2024-07-11 11:23:25 +0100

    kick off kit

--- src/Pages/SeeMore.tsx
+++ src/Pages/SeeMore.tsx
@@ -6,8 +6,11 @@ import { IoIosHeartEmpty } from "react-icons/io";
 import { IoIosHeart } from "react-icons/io";
 
 import { useProduct } from "../Context/ProductContext";
+import { useState } from "react";
 
 function SeeMore() {
+  const [curIndex, setCurIndex] = useState<number>(0);
+
   const {
     filteredProductData,
     productSelected,
@@ -17,18 +20,33 @@ function SeeMore() {
     handleLikes,
   } = useProduct();
 
+  const totalProducts = filteredProductData.length;
+
+  function prevSlide() {
+    setCurIndex((prevIndex) =>
+      prevIndex === 0 ? totalProducts - 1 : prevIndex - 1
+    );
+  }
+
+  function nextSlide() {
+    setCurIndex((prevIndex) =>
+      prevIndex === totalProducts - 1 ? 0 : prevIndex + 1
+    );
+  }
+
   return (
     <div className="see-more">
       <div className="head">
         <h3>You might also like</h3>
         <div className="button">
-          <TfiArrowCircleLeft className="icon" />
-          <TfiArrowCircleRight className="icon" />
+          <TfiArrowCircleLeft className="icon" onClick={prevSlide} />
+          <TfiArrowCircleRight className="icon" onClick={nextSlide} />
         </div>
       </div>
       <div className="product">
         {filteredProductData
-          .slice(0, desktopView ? 4 : 2)
+          // .slice(0, desktopView ? 4 : 2)
+          .slice(curIndex, desktopView ? curIndex + 4 : curIndex + 2)
           .map((product, index) => (
             <div key={index} className="product-container">
               <div className="img-container">
