318f4b38a9239021eb08064394bbfa23e6f4e33c
ifeanyihm <iheme.ifeanyi@yahoo.com>
2024-07-15 15:36:29 +0100

    kick off kit

--- src/Context/const Constant.tsx
+++ src/Context/const Constant.tsx
@@ -1,4 +1,4 @@
-export const API_KEY = import.meta.env.VITE_kICKOFFKIT_API_KEY;
-export const APP_ID = import.meta.env.VITE_kICKOFFKIT_API_ID;
+export const API_KEY = "69f5ec29c88d4f9f8e73cb33b62b279220240712125941210635";
+export const APP_ID = "YER5YP0BRIJRI5U";
 export const ID = "ad5291da29e04fb48ad03cf4fbdb0533";
 export const PAGE = 1;
